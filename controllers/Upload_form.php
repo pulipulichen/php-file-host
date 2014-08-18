a53fa9b93ec2a60bc87d39f6b65ee0e6419e4885
pudding <pudding@pudding-win7>
2014-08-18 22:44:56 +0800

    0818 弄了一個門面

--- controllers/Upload_form.php
+++ controllers/Upload_form.php
@@ -12,15 +12,31 @@ class Upload_form {
      * 顯示上傳檔案的位置
      * @version 20140818 Pulipuli Chen
      */
-    function display_view() {
+    function display_view($f3) {
         
         //$file = R::dispense("file");
         //$file->title="哈利波特";
         //$id = R::store($file);
+                
+        $this->get_how_to_use_content($f3);
+        $this->get_readme_update_year($f3);
         
         $template = new Template;
         echo $template->render("upload_form.html");
         
         //echo "upload form " . $id;
+    }
+    
+    private function get_how_to_use_content($f3) {
+        $content = "How to use content.";
+        
+        $f3->set('how_to_use_content', $content);
+        return $content;
+    }
+    
+    private function get_readme_update_year($f3) {
+        // @TODO get_readme_update_year
+        $year = '2014';
+        $f3->set('readme_update_year', $year);
     }
 }
---
+++ dict/en.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+    'upload_form' => array(
+        'title'=>'PHP File Host',
+        'form' => array(
+            'input_label'=> 'Upload Your File and Get a Link.',
+        ),
+        'how_to_use' => array(
+            'caption' => "How to Use",
+        ),
+        'footer' => array(
+            'author' => 'Pulipuli Chen',
+            'github' => 'PHP File Host@GitHub',
+            'blogger' => '布丁布丁吃什麼？',
+            'dlll' => 'DLLL'
+        )
+    )
+);
