6568602bf7ffe4f807947d5244b909291f7b4da7
pudding <pudding@pudding-win7>
2014-08-18 22:23:40 +0800

    0818 確認樣板功能，沒問題，很好上手

--- controllers/Upload_form.php
+++ controllers/Upload_form.php
@@ -7,12 +7,20 @@
  * @author pudding
  */
 class Upload_form {
+    
+    /**
+     * 顯示上傳檔案的位置
+     * @version 20140818 Pulipuli Chen
+     */
     function display_view() {
         
-        $file = R::dispense("file");
-        $file->title="哈利波特";
-        $id = R::store($file);
+        //$file = R::dispense("file");
+        //$file->title="哈利波特";
+        //$id = R::store($file);
         
-        echo "upload form " . $id;
+        $template = new Template;
+        echo $template->render("upload_form.html");
+        
+        //echo "upload form " . $id;
     }
 }
